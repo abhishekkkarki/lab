97a197c4793050492d0473e9de1e833e569ee40b
Abhishek Karki <94671116+abhishekkkarki@users.noreply.github.com>
2021-12-03 17:39:10 +0545

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,5 @@
+#find BMI of a person where take mass and height as input from the user
+mass=int(input("enter the mass in kg"))
+height=float(input("enter the height in m"))
+BMI=mass/ (height **2)
+print(f"BMI of a given data is {BMI}")
