0230f90eb49c87705e547737aa0f31bfe5796463
Abhishek Karki <94671116+abhishekkkarki@users.noreply.github.com>
2021-12-03 17:39:58 +0545

    Delete class.py

--- class.py
+++
@@ -1,8 +0,0 @@
-#convert seconds to day, hour and minutes
-seconds=int(input("enter the time in seconds"))
-days=seconds/86400
-hours=seconds/3600
-minutes=seconds/60
-print(f"total day for given seconds: {days}")
-print(f"total hours for given seconds: {hours}")
-print(f"total minutes for given seconds: {minutes}")
