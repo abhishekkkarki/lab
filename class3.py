de565cc96340107a0e8ddecc6665720cb48c918b
Abhishek Karki <94671116+abhishekkkarki@users.noreply.github.com>
2021-12-03 17:40:29 +0545

    Delete class3.py

--- class3.py
+++
@@ -1,6 +0,0 @@
-#take number of apples and people and divide accordingly
-apples= int(input("enter the number of apples"))
-people= int(input("enter the number of people"))
-divided= apples// people
-basket= apples% people
-print(f"each person gets {divided} apples and {basket} apples remaining in the basket")
